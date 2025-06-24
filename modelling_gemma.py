61a736b5fe21169979bfda8f752b52f4c60d1cb1
Muhamad Raka Pratama <raka824pratama@gmail.com>
2025-06-24 23:06:22 +0700

    creating RoPE

--- modelling_gemma.py
+++ modelling_gemma.py
@@ -146,6 +146,57 @@ def repeat_kv(hidden_states: torch.Tensor, n_rep: int) -> torch.Tensor:
     hidden_states = hidden_states[:, :, None, :, :].expand(batch, num_key_value_heads, n_rep, slen, head_dim)
     return hidden_states.reshape(batch, num_key_value_heads * n_rep, slen, head_dim)
 
+class GemmaRotaryEmbedding(nn.Module):
+    def __init__(self, dim, max_position_embeddings = 2048, base=10000, device=None):
+        super().__init__()
+        
+        self.dim = dim # sama kayak Head_Dim=256
+        self.max_position_embeddings = max_position_embeddings
+        self.base = base
+        
+        # calculate theta berdasarkan rumus theta RoPE. theta_i = base^(2i/dim) dimana i = 0, 1, 2, 3, ..., dim//2
+        inv_freq = 1.0 / (self.base ** (torch.arange(0, self.dim, 2, dtype=torch.int64).float() / self.dim))
+        self.register_buffer("inv_freq", tensor=inv_freq, persistent=False)
+    
+    @torch.no_grad()
+    def forward(self, x, position_ids, seq_len=None):
+        # x: [bs, num_attention_heads, seq_len, head_size]
+        self.inv_freq.to(x.device)
+        # Copy inv_freq tensor ke batch pada sequence
+        # inv_freq_expanded: [Batch_Size, Head_Dim // 2, 1]
+        inv_freq_expanded = self.inv_freq[None, :, None].float().expand(position_ids.shape[0], -1, 1)
+        # position_ids_expanded: [Batch_Size, 1, Seq_Len]
+        position_ids_expanded = position_ids[:, None, :].float()
+        device_type = x.device.type
+        device_type = device_type if isinstance(device_type, str) and device_type != "mps" else "cpu"
+        
+        with torch.autocast(device_type=device_type, enabled=False):
+            # Multiply masing2 theta dengan posisi-nya (argumen dari fungsi sinus dan cosinus)
+            # freqs: [Batch_Size, Head_Dim // 2, 1] @ [Batch_Size, 1, Seq_Len] -> [Batch_Size, Seq_Len, Head_Dim // 2]
+            freqs = (inv_freq_expanded.float() @ position_ids_expanded.float()).transpose(1, 2)
+            # emb: [Batch_Size, Seq_Len, Head_Dim]
+            emb = torch.cat((freqs, freqs), dim=-1)
+            # cos, sin: [Batch_Size, Seq_Len, Head_Dim]
+            cos = emb.cos()
+            sin = emb.sin()
+        
+        return cos.to(dtype=x.dtype), sin.to(dtype=x.dtype)
+
+def rotate_half(x):
+    # Membuat tensor yang sudah dirotasi [-x2, x1, -x4, x3, ...] di bagian "sin" (kalau dari paper, bagian kanan)
+    x1 = x[..., : x.shape[-1] // 2] # takes the first half dari dimensi
+    x2 = x[..., x.shape[-1] // 2 : ] # takes the second half dari dimensi
+    return torch.cat((-x2, x1), dim=-1)
+
+def apply_rotary_pos_emb(q, k, cos, sin, unsqueeze_dim=1):
+    cos = cos.unsqueeze(unsqueeze_dim) # menambahkan head dimension ke cos dan sin
+    sin = sin.unsqueeze(unsqueeze_dim) # menambahkan head dimension ke cos dan sin
+    # applying formula yang ada pada paper RoPE
+    q_embed = (q * cos) + (rotate_half(q) * sin)
+    k_embed = (k * cos) + (rotate_half(k) * sin)
+    
+    return q_embed, k_embed
+
 class GemmaAttention(nn.Module):
     def __init__(self, config: GemmaConfig, layer_idx: Optional[int] = None):
         super().__init__()
